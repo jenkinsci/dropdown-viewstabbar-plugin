b9a5380c934dd96fe1297a11a136e98fbf089cd4
Jesse Farinacci <jieryn@gmail.com>
2011-02-01 23:38:01 -0500

    first pass at l10n

--- src/main/java/hudson/views/tabbar/MessagesUtils.java
+++ src/main/java/hudson/views/tabbar/MessagesUtils.java
@@ -24,35 +24,43 @@
 
 package hudson.views.tabbar;
 
-import hudson.Extension;
-import hudson.views.ViewsTabBar;
-import hudson.views.ViewsTabBarDescriptor;
-
-import org.kohsuke.stapler.DataBoundConstructor;
-
 /**
- * This plugin provides an alternate rendering of the Views bar which runs along
- * the top of all views. This plugin is useful for instances which have a very
- * large number of views and want a compact rendering.
+ * Various utility functions for working with localized Messages.
  * 
  * @author <a href="mailto:jieryn@gmail.com">Jesse Farinacci</a>
  * @since 1.0
  */
-public final class DropDownViewsTabBar extends ViewsTabBar
+public final class MessagesUtils
 {
-  @DataBoundConstructor
-  public DropDownViewsTabBar()
-  {
-    super();
-  }
+  // /**
+  // * Format a system property.
+  // *
+  // * @param key
+  // * the key from the system property key=value pair
+  // * @param value
+  // * the value from the system property key=value pair
+  // * @return the formatted string
+  // *
+  // * @see Messages#DumpInfo_Environment_Variable(Object, Object)
+  // */
+  // public static String formatSystemProperty(final String key, final String
+  // value)
+  // {
+  // if (key == null)
+  // {
+  // return null;
+  // }
+  //
+  // // ---
+  //
+  // return Messages.DumpInfo_System_Property(key, value);
+  // }
 
-  @Extension
-  public static class DescriptorImpl extends ViewsTabBarDescriptor
+  /**
+   * Static-only access
+   */
+  private MessagesUtils()
   {
-    @Override
-    public String getDisplayName()
-    {
-      return Messages.DisplayName();
-    }
+    // static-only access
   }
 }
